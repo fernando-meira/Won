8ff2ac799ba53ab5bd479b2aa217458a12e6460f
fernando-meira <thomwork@outlook.com>
2021-09-28 23:51:19 -0300

    feat: Start MediaMatch component.

---
+++ src/components/MediaMatch/MediaMatch.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+
+import MediaMatch from '.';
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch,
+} as Meta;
+
+export const Basic: Story = () => <MediaMatch />;
