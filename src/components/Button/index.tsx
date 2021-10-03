50bd67005e39fff5f1982504c226997e2c331086
fernando-meira <thomwork@outlook.com>
2021-10-03 18:33:30 -0300

    Completed layout button.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,8 +2,13 @@ import * as S from './styles';
 
 export type ButtonProps = {
   children?: React.ReactNode;
+  size?: 'small' | 'medium' | 'large';
 };
 
-export default function Button({ children }: ButtonProps) {
-  return <S.Container>{!!children && <span>{children}</span>}</S.Container>;
+export default function Button({ children, size = 'medium' }: ButtonProps) {
+  return (
+    <S.Container size={size}>
+      {!!children && <span>{children}</span>}
+    </S.Container>
+  );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,35 @@
-import styled from 'styled-components';
+import styled, { css, DefaultTheme } from 'styled-components';
 
-export const Container = styled.button``;
+import { ButtonProps } from '.';
+
+type WrapperProps = Pick<ButtonProps, 'size'>;
+
+const containerModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `,
+};
+
+export const Container = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    border: none;
+    padding: ${theme.spacings.xxsmall};
+    border-radius: ${theme.border.radius};
+
+    color: ${theme.colors.white};
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+
+    ${!!size && containerModifiers[size](theme)};
+  `}
+`;
