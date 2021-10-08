132f0569761fa2b7ce1f9b1ad1deff1fdf93de5c
Fernando Meira <thomwork@outlook.com>
2021-10-07 23:50:56 -0300

    fix: storybook error.

--- src/components/Menu/Menu.stories.tsx
+++ src/components/Menu/Menu.stories.tsx
@@ -1,4 +1,5 @@
-import { Story, Meta } from '@storybook/react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
 import Menu from '.';
 
 export default {
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,31 +1,31 @@
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2';
-import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart';
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search';
 
-import { Logo } from '~/components';
+import Logo from 'components/Logo';
 
 import * as S from './styles';
 
-export default function Menu() {
-  return (
-    <S.Container>
+const Menu = () => (
+  <S.Container>
+    <S.IconWrapper>
+      <MenuIcon />
+    </S.IconWrapper>
+
+    <S.LogoWrapper>
+      <Logo hideOnMobile />
+    </S.LogoWrapper>
+
+    <S.MenuGroup>
       <S.IconWrapper>
-        <MenuIcon />
+        <SearchIcon />
       </S.IconWrapper>
 
-      <S.LogoWrapper>
-        <Logo hideOnMobile />
-      </S.LogoWrapper>
+      <S.IconWrapper>
+        <ShoppingCartIcon />
+      </S.IconWrapper>
+    </S.MenuGroup>
+  </S.Container>
+);
 
-      <S.MenuGroup>
-        <S.IconWrapper>
-          <SearchIcon />
-        </S.IconWrapper>
-
-        <S.IconWrapper>
-          <ShoppingCartIcon />
-        </S.IconWrapper>
-      </S.MenuGroup>
-    </S.Container>
-  );
-}
+export default Menu;
