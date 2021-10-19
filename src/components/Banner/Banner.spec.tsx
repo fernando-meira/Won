13b510d8c97cb440ab17a0a147e4f8ad190aab18
Fernando Meira <thomwork@outlook.com>
2021-10-19 00:23:39 -0300

    feat: started tests of the Banner component.

--- src/components/Banner/Banner.spec.tsx
+++ src/components/Banner/Banner.spec.tsx
@@ -1,10 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { renderWithTheme } from '~/utils/tests/helpers';
 
 import Banner from '.';
 
+const defaultProps = {
+  image: 'http://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death',
+};
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    // Veriricar se o title existe renderizado
+    const { debug, container } = renderWithTheme(<Banner {...defaultProps} />);
+
+    debug(container);
+
+    // Verificar se o title existe renderizado
     // Verificar se o subtitle existe renderizado
     // Verificar se a imagem existe renderizada
   });
